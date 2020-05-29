f8430ccd705096bccc371566149ba80b61a7e736
Kevin <fruitwasp@users.noreply.github.com>
2020-05-29 16:18:42 +0200

    Drops obsolete bc layer in di config for Symfony 4.1 and below

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,13 +18,7 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder(self::ROOT_NAME);
-
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root(self::ROOT_NAME);
-        }
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
